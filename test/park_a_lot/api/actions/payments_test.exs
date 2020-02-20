f7443dcab0b341897c821651f7462e62c5a6e871
l am not a Computer Scientist <david.c.h.cao@gmail.com>
2020-02-20 03:46:39 -0300

    [#3] Add Payments test cases

---
+++ test/park_a_lot/api/actions/payments_test.exs
@@ -0,0 +1,60 @@
+defmodule ParkaLot.API.Handlers.PaymentsTest do
+  use ExUnit.Case
+  use ParkaLot.RepoCase
+
+  alias ParkaLot.Entities
+  alias ParkaLot.Maybe
+  alias ParkaLot.Repo
+  alias ParkaLot.API.Handlers.Tickets
+  alias ParkaLot.API.Handlers.Payments
+
+  @endpoint "/api/tickets"
+
+  def create_new_ticket_handle() do
+    request = Raxx.request(:POST, "/api/tickets")
+    |> ParkaLot.API.set_json_payload(%{})  
+
+    Tickets.handle_request(request, %{})
+  end
+
+  def hit_payments_handle(barcode) do
+    request = Raxx.request(:POST, "/api/tickets/#{barcode}/payments")
+    |> ParkaLot.API.set_json_payload(%{"payment_method" => "cash"})  
+
+    Payments.handle_request(request, %{})
+  end
+  
+  test "when we hit endpoint payments endpoint , this must set the ticket as paid" do
+    response = create_new_ticket_handle() 
+
+    assert response.status == 200
+    assert {"content-type", "application/json"} in response.headers
+    assert {:ok, %{"data" => %{"id" => bardcode_hexa}}} = Jason.decode(response.body)
+    assert {ticket_id, ""} = Integer.parse(bardcode_hexa, 16)
+    assert is_integer(ticket_id)
+
+    response = hit_payments_handle(bardcode_hexa) 
+    assert {:ok, %{"data" => %{"paid" => true}}} = Jason.decode(response.body)
+  end
+
+  test "After we paid a ticket, this must cost 0" do
+    response = create_new_ticket_handle() 
+
+    assert response.status == 200
+    assert {"content-type", "application/json"} in response.headers
+    assert {:ok, %{"data" => %{"id" => bardcode_hexa}}} = Jason.decode(response.body)
+    assert {ticket_id, ""} = Integer.parse(bardcode_hexa, 16)
+    assert is_integer(ticket_id)
+
+    response = hit_payments_handle(bardcode_hexa) 
+    assert {:ok, %{"data" => %{"paid" => true}}} = Jason.decode(response.body)
+
+    cost_request = Raxx.request(:GET, "/api/tickets/#{bardcode_hexa}")
+    cost_response = Tickets.handle_request(cost_request, %{})
+    assert cost_response.status == 200
+    already_paid_ticket_cost = 0
+    assert {:ok, %{"data" => %{"cost" => already_paid_ticket_cost}}} = Jason.decode(cost_response.body)
+  end
+
+
+end
