79ef71a1b1493f385c12c48cdf8a32ddf4b2deec
l am not a Computer Scientist <david.c.h.cao@gmail.com>
2020-02-26 03:47:11 -0300

    [#5] Minimal clean code and refactor

--- lib/park_a_lot/api/handlers/payments.ex
+++ lib/park_a_lot/api/handlers/payments.ex
@@ -8,12 +8,12 @@ defmodule ParkaLot.API.Handlers.Payments do
   @impl Raxx.SimpleServer
 
   def handle_request(request = %{ path: ["api", "tickets", barcode, "payments"]}, _state) do
-    with {:ok, ticket_id} <-  Conversion.to_id_from(barcode),
-         {:ok, ejson} <- decode_body(request.body),
-         {:ok,  %{"payment_method" =>  pay_method}} <- validate_payment_method_in(ejson),
-         {:ok, ticket} <- Payments.pay_by(ticket_id, pay_method) do
-          response(:ok)
-          |> API.set_json_payload(%{data: ticket })
+    with  {:ok, ticket_id} <- Conversion.to_id_from(barcode),
+          {:ok, ejson} <- decode_body(request.body),
+          {:ok,  %{"payment_method" =>  pay_method}} <- validate_payment_method_in(ejson),
+          {:ok, ticket} <- Payments.pay_by(ticket_id, pay_method) do
+            response(:ok)
+            |> API.set_json_payload(%{data: ticket })
     else
       {:error, reason}  ->
         response(:ok)
@@ -26,18 +26,17 @@ defmodule ParkaLot.API.Handlers.Payments do
   def decode_body(body) do
     case String.length(body) do
       0 ->
-        Maybe.error("The Json body is mandatory")
+        Maybe.error("The Request's Json body is mandatory")
       _WhenOthers ->
         Jason.decode(body)
     end
   end
 
-    def validate_payment_method_in(ejson) do
-      case Map.keys(ejson) do
-        ["payment_method"] -> Maybe.ok(ejson)
-        _-> Maybe.error("The Json's payment_method field is mandatory")
+  def validate_payment_method_in(ejson) do
+    case Map.keys(ejson) do
+      ["payment_method"] -> Maybe.ok(ejson)
+        _-> Maybe.error("The payment_method field is mandatory")
       end
-
     end
 
 end
